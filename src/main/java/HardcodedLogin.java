1f6e14f8569b4064a1ef36100a662be3958adf30
Colby Canselo <colbycanselo@gmail.com>
2023-01-24 20:06:49 +0000

    Hardcoded Login

--- src/main/java/HardcodedLogin.java
+++ src/main/java/HardcodedLogin.java
@@ -1,5 +1,6 @@
 
 public class HardcodedLogin {
+
     /**
      * This challenge represents a hardcoded login example:
      *
@@ -7,14 +8,23 @@ public class HardcodedLogin {
      *     "admin" has a password of "qwerty".
      *     "user" has a password of "password".
      *
-     * The method should return true if the login should be permitted, and false if it should not. You will need to use
-     * if/else statements.
+     * The method should return true if the login should be permitted, and false if it should not. 
+     * You will need to use if/else statements.
      *
      * @param username a String that represents a username.
      * @param password a String that represents a password.
      * @return true if there is a successful login, and false otherwise.
      */
-    public boolean login(String username, String password){
+
+    public boolean login(String username, String password) {
+
+        if(username == "admin" && password == "qwerty") {
+            return true;
+        } else if(username == "user" && password == "password") {
+            return true;
+        }
+        
         return false;
     }
+
 }
